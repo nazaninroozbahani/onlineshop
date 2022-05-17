c7b98e13e42355c644fb0e9eced1680e0edbc269
Nazanin Roozbahani <roozbahani.n92@gmail.com>
2022-05-17 12:39:20 +0430

    feat: add google analytics

--- src/layout/header/Ads.js
+++ src/layout/header/Ads.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Header.module.scss";
 
 const Ads = () => {
-  return <button className={`${styles.ads} btn-primary mx-5`}>تبلیغ</button>;
+  return <button className="ads btn-primary mx-5">تبلیغ</button>;
 };
 
 export default Ads;
