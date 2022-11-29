b319d3d67c7a7c047a1ae17a517059647ee8cc93
Nazanin Roozbahani <roozbahani.n92@gmail.com>
2022-11-29 17:34:42 +0330

    Delete Ads.js

--- src/layout/header/Ads.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Header.module.scss";
-
-const Ads = () => {
-  return <button className="ads btn-primary mx-5">تبلیغ</button>;
-};
-
-export default Ads;
