9de826bb2471a29d7aae692f87c4e2fb4c8ba7b3
Nazanin Roozbahani <roozbahani.n92@gmail.com>
2022-11-29 17:34:10 +0330

    Update Header.js

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -2,17 +2,15 @@ import CartIcon from "./CartIcon";
 import Login from "./Login";
 import Logo from "./Logo";
 import React from 'react';
-import Searchhh from "./HeaderSearch";
+import Search from "./HeaderSearch";
 import styles from "./Header.module.scss"
-import Ads from "./Ads";
 
 const Header = () => {
 
     return (
         <div className={styles.header}>
             <Logo/>
-            <Searchhh/>
-            <Ads />
+            <Search/>
             <Login/>
             <CartIcon />
         </div>
